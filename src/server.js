36676213cee5e2f18ed3f18ddde9993fdb1efc15
PerdiDev <perdidev@gmail.com>
2023-07-25 18:55:30 -0700

    penanganan error dengan ext sever

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const albums = require('./api/albums');
 const AlbumService = require('./services/postgres/AlbumsService');
+const AlbumValidator = require('./validator/albums');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const albumsService = new AlbumService();
@@ -22,9 +24,37 @@ const init = async () => {
       plugin: albums,
       options: {
         service: albumsService,
+        validator: AlbumValidator,
       },
     },
   ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.statusCode(response.statusCode);
+        return newResponse;
+      }
+
+      if (!response.isServer) {
+        return h.continue;
+      }
+
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.statusCode(500);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
 
   await server.start();
   console.log(`HAVE FUN: Server berjalan pada ${server.info.uri}`);
