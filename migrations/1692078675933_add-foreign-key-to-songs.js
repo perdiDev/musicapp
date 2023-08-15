f1f9859756e57afc77d82a5f91d7a9db92afcf93
PerdiDev <perdidev@gmail.com>
2023-08-15 14:33:42 +0800

    add foreign key to songs.album_id

---
+++ migrations/1692078675933_add-foreign-key-to-songs.js
@@ -0,0 +1,15 @@
+exports.up = (pgm) => {
+  pgm.sql("INSERT INTO albums(id, name, year) VALUES('old_albums', 'old_albums', 2023)");
+
+  pgm.sql("UPDATE songs SET album_id='old_albums' WHERE album_id IS NULL");
+
+  pgm.addConstraint('songs', 'fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('songs', 'fk_songs.album_id_album.id');
+
+  pgm.sql("UPDATE songs SET album_id=NULL WHERE album_id='old_alums'");
+
+  pgm.sql("DELETE FROM albums WHERE id='old_albums'");
+};
